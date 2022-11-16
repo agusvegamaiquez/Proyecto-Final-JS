1e0159ec04dedc876d8cc0f968338de0fb583c22
Agustina Vega <avegamaiquez05@gmail.com>
2022-11-16 00:49:04 -0300

    Arreglando fotos

--- js/template.js
+++ js/template.js
@@ -1,13 +1,21 @@
 const retornoCard = (producto)=> {
-    return `<div class="card">
-                <div class="card-img-div" ><img class="card-image" src="${producto.imagen}"></div>
-                <div class="card-name">${producto.nombre}</div>
-                <div class="card-price">$ ${producto.precio}</div>
-                <div class="card-button">
-                    <button class="button button-outline button-add" id="${producto.nombre}" title="Clic para agregar el '${producto.nombre}' al carrito">+</button>
+    return `<div class="card mb-3" style="max-width: 540px;" >
+              <div class="row g-0">
+              <div class="col-md-4">
+                  <img src="${producto.imagen}" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${producto.nombre}</h5>
+                  <p class="card-text">$ ${producto.precio}</p>
+                    <div class="card-button">
+                      <button class="button button-outline button-add" id="${producto.nombre}" title="Clic para agregar el '${producto.nombre}' al carrito">+</button>
+                    </div>
                 </div>
+              </div>
+            </div>
             </div>`
-}
+          }
 
 const toast =(mensaje)=>{
     Toastify({
